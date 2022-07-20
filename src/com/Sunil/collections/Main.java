e8b314ab23a611ed3752a4321708b4e446bb7225
Sunil Ballani <ballanisunil123@gmail.com>
2022-07-20 16:08:28 +0530

    Done with Collections

---
+++ src/com/Sunil/collections/Main.java
@@ -0,0 +1,20 @@
+package com.Sunil.collections;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        List<Integer> list2 = new LinkedList<>();
+
+        List<Integer> vector = new Vector<>();
+
+        vector.add(45);
+        vector.add(5);
+        vector.add(15);
+        vector.add(56);
+
+        System.out.println(vector);
+
+    }
+}
